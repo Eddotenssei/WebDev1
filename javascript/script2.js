4ccc196f0fd0554cd5d1db449eb4801eb74ceccd
nikoloz11 <eddotenssei@gmail.com>
2026-01-22 20:00:03 +0400

    asd

---
+++ javascript/script2.js
@@ -0,0 +1,10 @@
+const score = prompt("Enter your score out of 100:");
+const grade = score / 10;
+
+if(grade > 9) {
+    alert("warmatebulad chabarebuli");
+} else if(grade > 5){
+    alert("chabarebuli");
+} else{
+    alert("chaiwerit");
+}
